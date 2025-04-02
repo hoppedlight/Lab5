697f4bbd7a1e38d667c431c83b4a1c0129d32cb3
hopped_light <hoppedlight@gmail.com>
2025-04-02 11:10:52 +0200

    commited changes

--- lab5.py
+++ lab5.py
@@ -1,6 +1,31 @@
-def add(x, y):
-  return x + y;
+import pytest
+from math_operations import add, multiply
 
-def test_add():
-  assert add(2, 3) == 5
-  assert add(-1, 1) == 0
+@pytest.fixture
+def sample_numbers():
+  return (3, 5)
+
+def test_add(sample_numbers):
+  a, b = sample_numbers
+  assert add(a, b) == 8
+
+def test_multiply(sample_numbers):
+  a, b = sample_numbers
+  assert multiply(a, b) == 15
+  
+@pytest.mark.parametrize("a, b, expected", [
+  (1, 2, 3),
+  (3, 3, 6),
+  (-1, 1, 0),
+  (0, 0, 0)
+])
+def test_add_param(a, b, expected):
+  assert add(a, b) == expected
+
+@pytest.mark.parametrize("a, b, expected", [
+  (2, 3, 6),
+  (0, 5, 0),
+  (-2, 3, -6)
+])
+def test_multiply_param(a, b, expected):
+  assert multiply(a, b) == expected
